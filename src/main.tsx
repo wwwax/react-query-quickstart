bd7858adcf8e420c54218419c89eb8b32fd6a141
Max Ivanov <waxivanov@gmail.com>
2023-07-16 15:39:32 +0300

    feat: add QueryProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import App from './App.tsx';
 
 import './index.css';
 
+const client = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={client}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
