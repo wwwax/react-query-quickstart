1f418b024a4a502f3da0c6698f4b60a5800617a7
Max Ivanov <waxivanov@gmail.com>
2023-07-22 01:12:48 +0300

    feat: toggle completed status

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,10 +1,19 @@
 import { ListItem, Stack, Checkbox } from '@chakra-ui/react';
 import { Todo } from '../types/todo';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toggleTodoStatus } from '../services/todos';
 
 const TodoItem: React.FC<Todo> = ({ id, title, completed }) => {
+  const client = useQueryClient();
+
+  const { mutate: toggle } = useMutation({
+    mutationFn: () => toggleTodoStatus(id, !completed),
+    onSuccess: () => client.invalidateQueries(['todos']), // completed status won't update without it
+  });
+
   return (
     <ListItem>
-      <Stack spacing={4} direction="row">
+      <Stack spacing={4} direction="row" onClick={() => toggle()}>
         <Checkbox isChecked={completed}>{title}</Checkbox>
       </Stack>
     </ListItem>
--- src/services/todos.ts
+++ src/services/todos.ts
@@ -13,3 +13,15 @@ export async function fetchTodos(state: TodoState = 'all'): Promise<Todo[]> {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return res.json();
 }
+
+export async function toggleTodoStatus(id: number, completed: boolean) {
+  const res = await fetch(`${BASE_URL}/${id}`, {
+    method: 'PATCH',
+    body: JSON.stringify({ completed }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return res.json();
+}
